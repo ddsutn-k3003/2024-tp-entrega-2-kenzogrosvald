ef5fffa13e8f77a66815d654e22ac61717278bbe
kenzogrosvald <groken@frba.utn.edu.ar>
2024-05-19 22:52:21 -0300

    Se actualiza pipeline y se remueve mock

--- src/main/java/ar/edu/utn/dds/k3003/clients/ViandasProxy.java
+++ src/main/java/ar/edu/utn/dds/k3003/clients/ViandasProxy.java
@@ -55,8 +55,6 @@ public class ViandasProxy implements FachadaViandas {
     @SneakyThrows
     @Override
     public ViandaDTO buscarXQR(String qr) throws NoSuchElementException {
-
-        /* Funciona prendiendo el servidor de Viandas Localmente
         Response<ViandaDTO> execute = service.get(qr).execute();
         if (execute.isSuccessful()) {
             return execute.body();
@@ -65,13 +63,14 @@ public class ViandasProxy implements FachadaViandas {
             throw new NoSuchElementException("No se encontro la vianda " + qr);
         }
         throw new RuntimeException("Error conectandose con el componente viandas");
-        */
 
+        /*
         if(!qr.equals("unQRQueExiste")){
             throw new NoSuchElementException("No se encontro la vianda " + qr);
         }
 
         return new ViandaDTO("unQRQueExiste", LocalDateTime.now(), EstadoViandaEnum.PREPARADA,1L,1);
+         */
 
     }
 
